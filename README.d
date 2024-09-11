d93d61f7019fa9000f3172adcd5d5425f1a5d972
PranitaJoshi1259 <pranitaaliteproject@gmail.com>
2024-09-11 12:26:55 +0530

    readme file

---
+++ README.d
@@ -0,0 +1,7 @@
+echo "# my-first-react-app" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/PranitaJoshi1259/my-first-react-app.git
+git push -u origin main
